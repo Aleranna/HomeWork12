4d75485ed6d03a6072451583f3f382310338ff38
Anna <aleranna22@gmail.com>
2022-08-24 23:23:32 +0300

    did my hw

--- functions.py
+++ functions.py
@@ -1,1 +1,30 @@
 import json
+POSTS_PATH = 'posts.json'
+
+
+def load_posts():
+    with open(POSTS_PATH, 'r', encoding='utf-8') as file:
+        return json.load(file)
+
+
+def get_posts_by_word(word: str) -> list[dict]:
+    result = []
+    for post in load_posts():
+        if word.lower() in post['content'].lower():
+            result.append(post)
+    return result
+
+
+def save_picture(picture):
+    filename = picture.filename
+    path = f'./uploads/images/{filename}'
+    picture.save(path)
+    return path
+
+
+def add_new_post(post: dict) -> dict:
+    posts = load_posts()
+    posts.append(post)
+    with open(POSTS_PATH, 'w', encoding='utf-8') as file:
+        json.dump(posts, file)
+    return post
